b51e6090bd65e12d83e429c7db1c919cc9ca9ec4
Chris Coe <chrcoe@ieee.org>
2015-08-08 21:02:59 -0400

    updated view1 tests and the view itself to get 5/5 tests passing

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -2,15 +2,19 @@
 
 describe('myApp.view1 module', function() {
 
-  beforeEach(module('myApp.view1'));
+    beforeEach(module('myApp.view1'));
 
-  describe('view1 controller', function(){
+    describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
-    }));
+        it('should be defined', inject(function($controller, $rootScope, $log, $http) {
+            //spec body
+            var view1Ctrl = $controller('View1Ctrl', {
+                '$scope': $rootScope.$new(),
+                '$log': $log,
+                '$http': $http
+            });
+            expect(view1Ctrl).toBeDefined();
+        }));
 
-  });
+    });
 });
